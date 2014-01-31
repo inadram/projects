5205cf7ca9b708c3c4cac17fa1a222821439db0f
inadram <inadram@gmail.com>
2014-01-31 09:27:53 +0000

    test subscribe span set expected attribute

--- chrome-extensions/iPlayer/src-test/inject/elementtest.js
+++ chrome-extensions/iPlayer/src-test/inject/elementtest.js
@@ -25,7 +25,7 @@ ElementTest.prototype.testCreateSubscribeDivShouldCreateLi = function () {
 };
 
 ElementTest.prototype.testCreateSubscribeDivShouldCreateLiWithExpectedIdAndClass = function () {
-	expectAsserts(1);
+	expectAsserts(3);
 
 	var documentStub = this._sandbox.stub(window.document);
 	var fakeDiv = {};
@@ -36,7 +36,32 @@ ElementTest.prototype.testCreateSubscribeDivShouldCreateLiWithExpectedIdAndClass
 	fakeDiv.cloneNode = function(){
 		return fakeLi;
 	};
-	documentStub.getElementsByClassName.returns([fakeDiv]);
+	documentStub.getElementsByClassName.withArgs('link-to-this').returns([fakeDiv]);
 	element.createSubscribe();
-	assertTrue(this._setAttributeSpy.called);
+	assertTrue(this._setAttributeSpy.withArgs('class','subscribe').called);
+	assertTrue(this._setAttributeSpy.withArgs('class','display-none').called);
+	assertTrue(this._setAttributeSpy.withArgs('id','subscribeLi').called);
+};
+
+ElementTest.prototype.testCreateSubscribeDivShouldCreateSubscribeSpanWithExpectedId = function () {
+	expectAsserts(1);
+
+	var documentStub = this._sandbox.stub(window.document);
+	var fakeDiv = {};
+	var fakeLi ={};
+	fakeLi.getElementsByTagName= function(){};
+	var fakeLiStub = this._sandbox.stub(fakeLi);
+	fakeDiv.cloneNode = function(){
+		return fakeLiStub;
+	};
+	documentStub.getElementsByClassName.withArgs('link-to-this').returns([fakeDiv]);
+
+
+
+	var fakeSpan = {};
+	fakeSpan.setAttribute = function(){};
+	var fakeSpanSetAttributeSpy = this._sandbox.spy(fakeSpan,'setAttribute');
+	fakeLiStub.getElementsByTagName.withArgs('span').returns([fakeSpan]);
+	element.createSubscribe();
+	assertTrue(fakeSpanSetAttributeSpy.withArgs('id','subscribeText').called);
 };
